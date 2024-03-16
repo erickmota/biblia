4f1310eccd5e74c32ae0986d5d7327d604780ecc
Erick <erick_fcsaopaulo@hotmail.com>
2024-03-15 23:55:56 -0300

    fiz o ajax funcionar

---
+++ ajax/busca.php
@@ -0,0 +1,9 @@
+<div>
+
+<p class="border-bottom pb-4">
+
+<b>Teste</b> - Teste
+
+</p>
+
+</div>
--- paginas/busca.php
+++ paginas/busca.php
@@ -26,6 +26,11 @@
     <!-- <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script> -->
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
+
+    <!-- <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
+    <link rel="stylesheet" href="/resources/demos/style.css">
+    <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
+    <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script> -->
     
 
     <?php
@@ -74,6 +79,8 @@
             <div class="col-12 col-lg-9">
 
                 <h3 class="mb-5">Buscando por: <?php echo $busca; ?></h3>
+                
+                <div id="espacoProdutos">
 
                 <?php
 
@@ -86,7 +93,7 @@
                 foreach ($classeCapitulo->buscaVerso() as $arrBusca){
                 
                 ?>
-                
+
                 <p class="border-bottom pb-4">
 
                     <b><?php echo $arrBusca["liv_nome"]; ?> <?php echo $arrBusca["ver_capitulo"]; ?>:<?php echo $arrBusca["ver_versiculo"]; ?></b> - <?php echo $arrBusca["ver_texto"]; ?>
@@ -101,7 +108,68 @@
                 
                 ?>
 
+                </div>
+
             </div>
+
+            <div class="col-12 col-lg-9 text-center mt-5">
+
+                <button class="btn btn-primary" id="btnMais">Carregar mais</button>
+
+            </div>
+
+            <script>
+
+                function retornaBusca() {
+                                            
+                    $.ajax({
+
+                        type: "POST",
+                        dataType: "html",
+
+                        url: "ajax/busca.php",
+
+                        /* beforeSend: function () {
+
+                            var pgMenosUm = parseInt(pg) - 1;
+
+                            $("#espacoProdutos"+pg).html("<img src='img/carregando.gif' width='60px'>");
+
+                        }, */
+
+                        /* data: {pg: pg, vMin: vMin, vMax: vMax, sexo: sexo, ordenacao: ordenacao, tamanho: tamanho, categoria: categoria, busca: busca}, */
+
+                        success: function (msg) {
+
+                            $("#espacoProdutos").append(msg);
+
+                            $("#botaoMostrar").removeClass("d-none");
+                            $("#imgLoading").addClass("d-none");
+
+                        }
+
+                    });
+
+                }
+
+                $("#btnMais").click(function(){
+
+                    $("#btnMais").addClass("d-none");
+                    /* $("#imgLoading").removeClass("d-none"); */
+
+                    /* var pg = document.getElementById("hiddenPg").value; */
+
+                    retornaBusca();
+
+                    /* $("#hiddenPg").val(parseInt(pg) + 1); */
+
+                    /* var pgMenosUm = parseInt(pg) - 1; */
+
+                    /* $("<div class='row' id='espacoProdutos"+pg+"'></div>").insertAfter("#espacoProdutos"+pgMenosUm); */
+
+                });
+
+            </script>
 
         </div>
 
